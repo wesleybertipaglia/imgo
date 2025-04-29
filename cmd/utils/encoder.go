9ac2d92b31eeae9b1609f06d9901b866ce0667e4
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-04-28 22:01:56 -0300

    feat: implement GetEncoder function for image encoding and remove obsolete encoder_factory

--- cmd/utils/encoder.go
+++ cmd/utils/encoder.go
@@ -1,10 +1,26 @@
 package utils
 
 import (
+	"errors"
 	"image"
+	"imgo/cmd/infra"
 	"io"
+	"strings"
 )
 
 type ImageEncoder interface {
 	Encode(w io.Writer, img image.Image, quality int) error
+}
+
+func GetEncoder(format string) (ImageEncoder, error) {
+	switch strings.ToLower(format) {
+	case "webp":
+		return infra.WebpEncoder{}, nil
+	case "jpg", "jpeg":
+		return infra.JpegEncoder{}, nil
+	case "png":
+		return infra.PngEncoder{}, nil
+	default:
+		return nil, errors.New("unsupported format: " + format)
+	}
 }
